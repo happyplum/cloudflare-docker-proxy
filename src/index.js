752c96de571103c72b66c203f4ec2d9c0cd68cf4
李振宇 <lizhenyu@joyware.com>
2024-09-04 10:02:08 +0800

    chang domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.happyplum.cn": dockerHub,
+  "quay.happyplum.cn": "https://quay.io",
+  "gcr.happyplum.cn": "https://gcr.io",
+  "k8s-gcr.happyplum.cn": "https://k8s.gcr.io",
+  "k8s.happyplum.cn": "https://registry.k8s.io",
+  "ghcr.happyplum.cn": "https://ghcr.io",
+  "cloudsmith.happyplum.cn": "https://docker.cloudsmith.io",
+  "ecr.happyplum.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.happyplum.cn": dockerHub,
 };
 
 function routeByHosts(host) {
